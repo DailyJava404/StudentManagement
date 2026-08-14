0f05bd297fc68caa3f691a2b98a01ddf074e32d9
ewent404 <ewentfirex@gmail.com>
2026-08-14 14:04:53 +0700

    feat(student): remove key from application.properties

--- src/main/java/com/example/studentsmanagement/Model/Request/SendMessageTelegramRequest.java
+++ src/main/java/com/example/studentsmanagement/Model/Request/SendMessageTelegramRequest.java
@@ -1,4 +1,13 @@
 package com.example.studentsmanagement.Model.Request;
 
-public class SendMessageTelegramRequest {
-}
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+
+public record SendMessageTelegramRequest(
+        @JsonProperty("chat_id")
+        @NotBlank(message = "chatId must not be empty")
+        String chatId,
+
+        @NotBlank(message = "text must not be empty")
+        String text
+) {}
