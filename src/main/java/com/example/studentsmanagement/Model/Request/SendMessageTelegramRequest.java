c550a43ec6cc0d407aa5d5104cbc2b13ed431086
ewent404 <ewentfirex@gmail.com>
2026-08-14 14:00:09 +0700

    Stop tracking secrets file

--- src/main/java/com/example/studentsmanagement/Model/Request/SendMessageTelegramRequest.java
+++ src/main/java/com/example/studentsmanagement/Model/Request/SendMessageTelegramRequest.java
@@ -1,13 +1,4 @@
 package com.example.studentsmanagement.Model.Request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotBlank;
-
-public record SendMessageTelegramRequest(
-        @JsonProperty("chat_id")
-        @NotBlank(message = "chatId must not be empty")
-        String chatId,
-
-        @NotBlank(message = "text must not be empty")
-        String text
-) {}
+public class SendMessageTelegramRequest {
+}
